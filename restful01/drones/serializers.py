62422966ba42ec3ad64d9e49251fcc8680d87316
alan-nunes <201911250008@ifba.edu.br>
2024-09-13 22:34:03 -0300

    first commit

---
+++ restful01/drones/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from drones.models import DronesCategory, Drone, Competition, Pilot
+
+class DroneCategorySerializer(serializers.HyperlinkedModelSerializer):
+    drones = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name="drone-detail")
+    
+    class Meta:
+        model = DronesCategory
+        fields = ("url", "pk", "name", "drones")
+        
+class DroneSerializer(serializers.HyperlinkedModelSerializer):
+    drone_category = serializers.SlugRelatedField(
+        queryset=DronesCategory.objects.all(), slug_field="name"
+    )
+
+    class Meta:
+        model = Drone
+        fields = (
+            "url",
+            "name",
+            "drone_category",
+            "manufacturing_date",
+            "has_it_completed",
+            "inserted_timestamp",
+        )
+class CompetitionSerializer(serializers.HyperlinkedModelSerializer):
+    drone = DroneSerializer()
+    
+    class Meta:
+        model = Competition
+        fields = ("url", "pk", "distance_in_feet", "distance_achievement_date", "drone")
+        
+class PilotSerializer(serializers.HyperlinkedModelSerializer):
+    competitions = CompetitionSerializer(many=True, read_only=True)
+    gender = serializers.ChoiceField(choices=Pilot.GENDER_CHOICES)
+    gender_descripiton = serializers.CharField(source="get_gender_display", read_only=True)
+    
+    class Meta:
+        model = Pilot
+        fields = (
+            "url",
+            "name",
+            "gender",
+            "gender_description",
+            "races_count",
+            "inserted_timestamp",
+            "competitions",
+        )
+
+class PilotCompetitonSerializer(serializers.ModelSerializer):
+    pilot = serializers.SlugRelatedField(queryset=Pilot.objects.all(), slug_field="name")
+    
+    drone = serializers.SlugRelatedField(queryset=Drone.objects.all(), slug_field="name")
+    
+    class Meta:
+        model = Competition
+        fields = (
+            "url",
+            "pk",
+            "distance_in_feet",
+            "distance_achievement_date",
+            "pilot",
+            "drone",
+        )
+    
